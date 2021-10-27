51de8288c4ed3734ca923c70d05e87e6b6bc7c5d
Julio Cervantes <65304872+JCervantesB@users.noreply.github.com>
2021-10-27 16:14:07 -0600

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,19 +1,146 @@
-document.addEventListener('DOMContentLoaded', function() {
-    iniciarApp();
-});
-
-function iniciarApp() {
-    consultarAPI(); // Consulta el API del backend PHP    
-}
-
-async function consultarAPI() {
-    try {
-        const url = 'http://198.251.74.213:10012/api/equipos';
-        const resultado = await fetch(url);
-        const equipos = await resultado.json();
-        mostrarEquipos(equipos);          
-
-    } catch (error){
-        console.log(error);
-    }
-}
+autos = [
+  {
+    "id": 99,
+    "code": "TIC/PC/0001",
+    "name": "[PC]Contador2",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 100,
+    "code": "TIC/PC/0002",
+    "name": "[PC]Contador3",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 101,
+    "code": "TIC/PC/0003",
+    "name": "[PC]Contador4",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 102,
+    "code": "TIC/PC/0004",
+    "name": "[PC]AuxConta1",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 103,
+    "code": "TIC/PC/0005",
+    "name": "[PC]AuxConta3",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 104,
+    "code": "TIC/PC/0006",
+    "name": "[PC]AuxConta6",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 105,
+    "code": "TIC/PC/0007",
+    "name": "[PC]Tesorería/baja",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 106,
+    "code": "TIC/PC/0008",
+    "name": "[PC]CréditoyCobranza",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 107,
+    "code": "TIC/PC/0009",
+    "name": "[PC]AsesorVentas1",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 108,
+    "code": "TIC/PC/0010",
+    "name": "[PC]AsesorVentas2",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 109,
+    "code": "TIC/PC/0011",
+    "name": "[PC]AsesorVentas3",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 110,
+    "code": "TIC/PC/0012",
+    "name": "[PC]Recepción",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 111,
+    "code": "TIC/PC/0013",
+    "name": "[PC]AdmónVentas",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 112,
+    "code": "TIC/PC/0014",
+    "name": "[PC]Sucursal1",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 113,
+    "code": "TIC/PC/0015",
+    "name": "[PC]Asistente",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  },
+  {
+    "id": 114,
+    "code": "TIC/PC/0017",
+    "name": "[PC]AuxRiesgos",
+    "category_id": [
+      5,
+      "Computadora de Escritorio"
+    ]
+  }
+]
