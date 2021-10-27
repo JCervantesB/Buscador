1c71b9314f5a54acb104f18b61426c3622a7a356
Julio Cervantes <65304872+JCervantesB@users.noreply.github.com>
2021-10-27 16:07:37 -0600

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,174 +1,19 @@
-const autos = [
-	{
-		marca: 'BMW',
-		modelo: 'Serie 3',
-		year: 2020,
-		precio: 30000,
-		puertas: 4,
-		color: 'Blanco',
-		transmision: 'automatico'
-	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A4', 
-        year: 2020, 
-        precio: 40000, 
-        puertas: 4, 
-        color: 'Negro', 
-        transmision: 'automatico' 
-    },
-	{
-		marca: 'Ford',
-		modelo: 'Mustang',
-		year: 2015,
-		precio: 20000,
-		puertas: 2,
-		color: 'Blanco',
-		transmision: 'automatico'
-	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A6', 
-        year: 2020, 
-        precio: 35000, 
-        puertas: 4, 
-        color: 'Negro', 
-        transmision: 'automatico' 
-    },
-	{
-		marca: 'BMW',
-		modelo: 'Serie 5',
-		year: 2016,
-		precio: 70000,
-		puertas: 4,
-		color: 'Rojo',
-		transmision: 'automatico'
-	},
-	{
-		marca: 'Mercedes Benz',
-		modelo: 'Clase C',
-		year: 2015,
-		precio: 25000,
-		puertas: 4,
-		color: 'Blanco',
-		transmision: 'automatico'
-	},
-	{
-		marca: 'Chevrolet',
-		modelo: 'Camaro',
-		year: 2018,
-		precio: 60000,
-		puertas: 2,
-		color: 'Rojo',
-		transmision: 'manual'
-	},
-	{ 
-        marca: 'Ford', 
-        modelo: 'Mustang', 
-        year: 2019, 
-        precio: 80000, 
-        puertas: 2, 
-        color: 'Rojo', 
-        transmision: 'manual' 
-    },
-	{
-		marca: 'Dodge',
-		modelo: 'Challenger',
-		year: 2020,
-		precio: 40000,
-		puertas: 4,
-		color: 'Blanco',
-		transmision: 'automatico'
-	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A3', 
-        year: 2017, 
-        precio: 55000, 
-        puertas: 2, 
-        color: 'Negro', 
-        transmision: 
-        'manual' 
-    },
-	{
-		marca: 'Dodge',
-		modelo: 'Challenger',
-		year: 2020,
-		precio: 25000,
-		puertas: 2,
-		color: 'Rojo',
-		transmision: 'manual'
-	},
-	{
-		marca: 'Mercedes Benz',
-		modelo: 'Clase C',
-		year: 2018,
-		precio: 45000,
-		puertas: 4,
-		color: 'Azul',
-		transmision: 'automatico'
-	},
-	{
-		marca: 'BMW',
-		modelo: 'Serie 5',
-		year: 2019,
-		precio: 90000,
-		puertas: 4,
-		color: 'Blanco',
-		transmision: 'automatico'
-	},
-	{ 
-        marca: 'Ford', 
-        modelo: 'Mustang', 
-        year: 2017, 
-        precio: 60000, 
-        puertas: 2, 
-        color: 'Negro', 
-        transmision: 'manual' 
-    },
-	{
-		marca: 'Dodge',
-		modelo: 'Challenger',
-		year: 2015,
-		precio: 35000,
-		puertas: 2,
-		color: 'Azul',
-		transmision: 'automatico'
-	},
-	{
-		marca: 'BMW',
-		modelo: 'Serie 3',
-		year: 2018,
-		precio: 50000,
-		puertas: 4,
-		color: 'Blanco',
-		transmision: 'automatico'
-	},
-	{
-		marca: 'BMW',
-		modelo: 'Serie 5',
-		year: 2017,
-		precio: 80000,
-		puertas: 4,
-		color: 'Negro',
-		transmision: 'automatico'
-	},
-	{
-		marca: 'Mercedes Benz',
-		modelo: 'Clase C',
-		year: 2018,
-		precio: 40000,
-		puertas: 4,
-		color: 'Blanco',
-		transmision: 'automatico'
-	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A4', 
-        year: 2016, 
-        precio: 30000, 
-        puertas: 4, 
-        color: 'Azul', 
-        transmision: 'automatico' 
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    consultarAPI(); // Consulta el API del backend PHP    
+}
+
+async function consultarAPI() {
+    try {
+        const url = 'http://198.251.74.213:10012/api/equipos';
+        const resultado = await fetch(url);
+        const equipos = await resultado.json();
+        mostrarEquipos(equipos);          
+
+    } catch (error){
+        console.log(error);
     }
-];
+}
